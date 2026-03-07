5f12d193e9dcddb7e75f6f8276fd8810b568029d
adonicristiangerman <adoni.cristiang@gmail.com>
2026-03-07 17:25:29 -0400

    Changed the state to optain the player information

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import Player from "./components/Player";
-import GameBoard from "./components/GameBoard";
 import { useState } from "react";
 
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
+
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((currentActivePlayer) =>
       currentActivePlayer === "X" ? "O" : "X",
     );
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "0";
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -31,6 +48,8 @@ function App() {
           activePlayerSymbol={activePlayer}
         />
       </div>
+
+      <Log />
     </main>
   );
 }
